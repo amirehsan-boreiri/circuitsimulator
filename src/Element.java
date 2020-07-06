ef3e0694c9ada0820a7d3a975218016afae5d386
amirehsan-boreiri <67389409+amirehsan-boreiri@users.noreply.github.com>
2020-07-07 00:06:32 +0430

    final

--- src/Element.java
+++ src/Element.java
@@ -1,7 +1,8 @@
 public class Element {
     Element (){}
-    double v1,v2,deltaV=v2-v1;
+    //double v1,v2,deltaV=v2-v1;
     double i;
     double power;
-    //test ham zamani
+    Node n1,n2;
+    double delta=n1.V-n2.V;
 }
---
+++ src/Node.java
@@ -0,0 +1,15 @@
+public class Node {
+    String name;
+    double V,Vghabl;
+    boolean visited;
+    //very important
+    Node(){}
+    double[] voltages; //save all steps --> arrayList
+    boolean checkGraph;
+    Element[] eleman;//connected elements to this node -->arrayList
+    //hashmap:
+    //var --> save neighbor nodes
+    //resetNode --> visited ro 0 mikone
+    //checking errors -->
+
+}
